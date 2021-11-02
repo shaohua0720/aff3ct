7ee15c3fd907c99692a664ff5ac8971dc8c8c72a
shaohua <shaohua@ustb.edu.cn>
2021-11-02 18:42:08 +0800

    otfs.

--- include/Module/Waveform/OTFS/Otfs.hpp
+++ include/Module/Waveform/OTFS/Otfs.hpp
@@ -2,6 +2,8 @@
 #define OTFS_HPP_
 
 #include <Module/Waveform/Ofdm.hpp>
+#include <Tools/Math/matrix.h>
+#include <fftw3.h>
 
 namespace aff3ct
 {
@@ -11,11 +13,24 @@ namespace module
     class Otfs: public Ofdm<B>
     {
     private:
+        fftw_complex *isfft_data_inplace;
+        fftw_plan isfft_doppler;
+        fftw_plan isfft_delay;
+
+        fftw_complex *sfft_data_inplace;
+        fftw_plan sfft_doppler;
+        fftw_plan sfft_delay;
+
     public:
         Otfs(int M, const std::vector<int> cp, int N);
         virtual ~Otfs();
-        void modulate(const B *X_K, B *Y_K,int frame_id = -1);
-        void demodulate(const B *X_K, B *Y_K,int frame_id = -1);
+        
+        // data in X_K is column-major, which is M x N
+        void modulate(const B *X_K, B *Y_K,int frame_id = -1); 
+        void demodulate(const B *X_K, B *Y_K,int frame_id = -1); 
+    protected:
+        void _isfft(const B *X_K, B *Y_K); // IFFT for doppler(row), FFT for delay(col)
+        void _sfft(const B *X_K, B *Y_K); // FFT for doppler(row), IFFT for delay(col)
     };
     
 }
--- include/Module/Waveform/OTFS/Otfs.hxx
+++ include/Module/Waveform/OTFS/Otfs.hxx
@@ -1,4 +1,6 @@
 #include "Module/Waveform/OTFS/Otfs.hpp"
+#include <complex>
+#include <eigen3/Eigen/Eigen>
 #include <string>
 #include <iostream>
 
@@ -11,22 +13,61 @@ namespace aff3ct
         {
             const std::string name = "OTFS";
             this->set_name(name);
+
+            isfft_data_inplace = (fftw_complex *)fftw_malloc(M*N*sizeof(fftw_complex));
+            /*N-points IFFT for doppler*/
+            isfft_doppler = fftw_plan_many_dft(1,&N,M,isfft_data_inplace,NULL,M,1,isfft_data_inplace,NULL,M,1,FFTW_BACKWARD,FFTW_MEASURE);
+            /*M-points FFT for delay*/
+            isfft_delay = fftw_plan_many_dft(1,&M,N,isfft_data_inplace,NULL,1,M,isfft_data_inplace,NULL,1,M,FFTW_FORWARD,FFTW_MEASURE);
+            
+            sfft_data_inplace = (fftw_complex *)fftw_malloc(M*N*sizeof(fftw_complex));
+            /*N-points FFT for doppler*/
+            sfft_doppler = fftw_plan_many_dft(1,&N,M,sfft_data_inplace,NULL,M,1,sfft_data_inplace,NULL,M,1,FFTW_FORWARD,FFTW_MEASURE);
+            /*M-points IFFT for delay*/
+            sfft_delay = fftw_plan_many_dft(1,&M,N,sfft_data_inplace,NULL,1,M,sfft_data_inplace,NULL,1,M,FFTW_BACKWARD,FFTW_MEASURE);
+
         }
         
         template <typename B>
         Otfs<B>::~Otfs()
         {
-
+            fftw_destroy_plan(isfft_delay);
+            fftw_destroy_plan(isfft_doppler);
+            fftw_destroy_plan(sfft_delay);
+            fftw_destroy_plan(sfft_doppler);
+            fftw_free(isfft_data_inplace);
+            fftw_free(sfft_data_inplace);
         }
         template <typename B>
         void Otfs<B>::modulate(const B *X_K, B *Y_K,int frame_id)
         {
-
+            B data[this->M*this->N*sizeof(fftw_complex)];
+            this->_isfft(X_K,data); // ISFFT transform
+            this->_ifft(data,Y_K); // Heisenberg transform
         }
         template <typename B>
         void Otfs<B>::demodulate(const B *X_K, B *Y_K,int frame_id)
         {
+            B data[this->M*this->N*sizeof(fftw_complex)];
+            this->_fft(X_K,data);  //wiener transform 
+            this->_sfft(data,Y_K);  // SFFT transform
+        }
 
+        template <typename B>
+        void Otfs<B>::_isfft(const B *X_K, B *Y_K) // IFFT for doppler(row), FFT for delay(col)
+        {
+            memcpy(isfft_data_inplace,X_K,this->M*this->N*sizeof(fftw_complex));
+            fftw_execute(isfft_doppler);
+            fftw_execute(isfft_delay);
+            memcpy(Y_K,isfft_data_inplace,this->M*this->N*sizeof(fftw_complex));
+        }
+        template <typename B>
+        void Otfs<B>::_sfft(const B *X_K, B *Y_K) // FFT for doppler(row), IFFT for delay(col)
+        {  
+            memcpy(sfft_data_inplace,X_K,this->M*this->N*sizeof(fftw_complex));
+            fftw_execute(sfft_doppler);
+            fftw_execute(sfft_delay);
+            memcpy(Y_K,sfft_data_inplace,this->M*this->N*sizeof(fftw_complex));
         }
     }
 
